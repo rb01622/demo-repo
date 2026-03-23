fdf679cb8e26649d3d28c8c71dce596a97aec8a6
rb01622 <kbkannah@gmail.com>
2026-03-23 13:30:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 
-  res.send('Hello Rajesh kannah K CI/CD World 🚀');
+  res.send('Hello Rajesh kannah CI/CD World 🚀');
 });
 
 app.listen(PORT, () => {
